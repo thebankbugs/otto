b1591b664c975fa2ebeb8374622b6c42bd5bd89d
thebankbugs <thebankbugs@gmail.com>
2026-06-18 20:15:50 +1000

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const premiumUserDirectory = [
     { email: "admin@trading.com", password: "tradeking2026" },
     { email: "trader1", password: "vipaccess2026" },
     { email: "member2@docs.com", password: "mypassword99" },
-    { email: "member2@doc.com", password: "mypassword9" }
+    { email: "member2@doc.com", password: "mypassword9" },
+    { email: "admin@thebankbugs.app", password: "ddfee773429a" }
     // You can manually drop new premium user combinations right here!
 ];
 
