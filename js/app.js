184aedaa85d739e08c1e1b4367e5477e6aec2dff
thebankbugs <thebankbugs@gmail.com>
2026-07-02 05:21:00 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -283,3 +283,61 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+splash 
+
+const messages = [
+
+    "Initializing TheBankBugs...",
+
+    "Loading Trading Academy...",
+
+    "Preparing Starter Dashboard...",
+
+    "Loading Elite Experience...",
+
+    "Syncing Platform...",
+
+    "Ready..."
+
+];
+
+const loadingText = document.getElementById("loading-text");
+
+let index = 0;
+
+const interval = setInterval(() => {
+
+    index++;
+
+    if(index < messages.length){
+
+        loadingText.textContent = messages[index];
+
+    }
+
+},500);
+
+setTimeout(()=>{
+
+    clearInterval(interval);
+
+    document.body.style.opacity="0";
+
+    document.body.style.transition="opacity .6s";
+
+    setTimeout(()=>{
+
+        window.location.href="pages/welcome.html";
+
+    },600);
+
+},3500);
+
+setTimeout(() => {
+  document.body.classList.add("fade-out");
+
+  setTimeout(() => {
+    window.location.href = "pages/welcome.html";
+  }, 800);
+}, 3000);
